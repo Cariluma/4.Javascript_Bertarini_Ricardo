96fd8097e93968f77276da7e4f302b4d56c0097c
Ricardo_Bertarini <bertarini@gmail.com>
2017-05-02 20:33:49 -0300

    Instrucciones 3

--- codBase/js/app1.js
+++ codBase/js/app1.js
@@ -1,14 +1,159 @@
 var calculadora = (function(){
 alert("Funciona");
 
-function cambioteclamas(){
+function cambiomas(){
   document.getElementById('mas').style="width:90%;height:98%"
 }
-function teclainiciomas(){
-  document.getElementById('mas').style="width:90%;height:100%"
+function cambioon(){
+  document.getElementById('on').style="width:77.88px;height:59.90px"
+}
+function cambiosign(){
+  document.getElementById('sign').style="width:77.88px;height:59.90px"
+}
+function cambioraiz(){
+  document.getElementById('raiz').style="width:77.88px;height:59.90px"
+}
+function cambiodividido(){
+  document.getElementById('dividido').style="width:77.88px;height:59.90px"
+}
+function cambiopor(){
+  document.getElementById('por').style="width:77.88px;height:59.90px"
+}
+function cambiomenos(){
+  document.getElementById('menos').style="width:77.88px;height:59.90px"
+}
+function cambiopunto(){
+  document.getElementById('punto').style="width:77.88px;height:59.90px"
+}
+function cambioigual(){
+  document.getElementById('igual').style="width:77.88px;height:59.90px"
+}
+function cambio1(){
+  document.getElementById('1').style="width:77.88px;height:59.90px"
+}
+function cambio2(){
+  document.getElementById('2').style="width:77.88px;height:59.90px"
+}
+function cambio3(){
+  document.getElementById('3').style="width:77.88px;height:59.90px"
+}
+function cambio4(){
+  document.getElementById('4').style="width:77.88px;height:59.90px"
+}
+function cambio5(){
+  document.getElementById('5').style="width:77.88px;height:59.90px"
+}
+function cambio6(){
+  document.getElementById('6').style="width:77.88px;height:59.90px"
+}
+function cambio7(){
+  document.getElementById('7').style="width:77.88px;height:59.90px"
+}
+function cambio8(){
+  document.getElementById('8').style="width:77.88px;height:59.90px"
+}
+function cambio9(){
+  document.getElementById('9').style="width:77.88px;height:59.90px"
+}
+function cambio0(){
+  document.getElementById('0').style="width:77.88px;height:59.90px"
 }
 
-document.getElementById('mas').onmousedown=cambioteclamas;
-document.getElementById('mas').onmouseup=teclainiciomas;
+function iniciomas(){
+  document.getElementById('mas').style="width:90%;height:100%"
+}
+function inicioon(){
+  document.getElementById('on').style="width:77.88px;height:62.90px"
+}
+function iniciosign(){
+  document.getElementById('sign').style="width:77.88px;height:62.90px"
+}
+function inicioraiz(){
+  document.getElementById('raiz').style="width:77.88px;height:62.90px"
+}
+function iniciodividido(){
+  document.getElementById('dividido').style="width:77.88px;height:62.90px"
+}
+function iniciopor(){
+  document.getElementById('por').style="width:77.88px;height:62.90px"
+}
+function iniciomenos(){
+  document.getElementById('menos').style="width:77.88px;height:62.90px"
+}
+function iniciopunto(){
+  document.getElementById('punto').style="width:77.88px;height:62.90px"
+}
+function inicioigual(){
+  document.getElementById('igual').style="width:77.88px;height:62.90px"
+}
+function inicio1(){
+  document.getElementById('1').style="width:77.88px;height:62.90px"
+}
+function inicio2(){
+  document.getElementById('2').style="width:77.88px;height:62.90px"
+}
+function inicio3(){
+  document.getElementById('3').style="width:77.88px;height:62.90px"
+}
+function inicio4(){
+  document.getElementById('4').style="width:77.88px;height:62.90px"
+}
+function inicio5(){
+  document.getElementById('5').style="width:77.88px;height:62.90px"
+}
+function inicio6(){
+  document.getElementById('6').style="width:77.88px;height:62.90px"
+}
+function inicio7(){
+  document.getElementById('7').style="width:77.88px;height:62.90px"
+}
+function inicio8(){
+  document.getElementById('8').style="width:77.88px;height:62.90px"
+}
+function inicio9(){
+  document.getElementById('9').style="width:77.88px;height:62.90px"
+}
+function inicio0(){
+  document.getElementById('0').style="width:77.88px;height:62.90px"
+}
+
+document.getElementById('mas').onmousedown=cambiomas;
+document.getElementById('mas').onmouseup=iniciomas;
+document.getElementById('on').onmousedown=cambioon;
+document.getElementById('on').onmouseup=inicioon;
+document.getElementById('sign').onmousedown=cambiosign;
+document.getElementById('sign').onmouseup=iniciosign;
+document.getElementById('raiz').onmousedown=cambioraiz;
+document.getElementById('raiz').onmouseup=inicioraiz;
+document.getElementById('dividido').onmousedown=cambiodividido;
+document.getElementById('dividido').onmouseup=iniciodividido;
+document.getElementById('por').onmousedown=cambiopor;
+document.getElementById('por').onmouseup=iniciopor;
+document.getElementById('menos').onmousedown=cambiomenos;
+document.getElementById('menos').onmouseup=iniciomenos;
+document.getElementById('punto').onmousedown=cambiopunto;
+document.getElementById('punto').onmouseup=iniciopunto;
+document.getElementById('igual').onmousedown=cambioigual;
+document.getElementById('igual').onmouseup=inicioigual;
+document.getElementById('1').onmousedown=cambio1;
+document.getElementById('1').onmouseup=inicio1;
+document.getElementById('2').onmousedown=cambio2;
+document.getElementById('2').onmouseup=inicio2;
+document.getElementById('3').onmousedown=cambio3;
+document.getElementById('3').onmouseup=inicio3;
+document.getElementById('4').onmousedown=cambio4;
+document.getElementById('4').onmouseup=inicio4;
+document.getElementById('5').onmousedown=cambio5;
+document.getElementById('5').onmouseup=inicio5;
+document.getElementById('6').onmousedown=cambio6;
+document.getElementById('6').onmouseup=inicio6;
+document.getElementById('7').onmousedown=cambio7;
+document.getElementById('7').onmouseup=inicio7;
+document.getElementById('8').onmousedown=cambio8;
+document.getElementById('8').onmouseup=inicio8;
+document.getElementById('9').onmousedown=cambio9;
+document.getElementById('9').onmouseup=inicio9;
+document.getElementById('0').onmousedown=cambio0;
+document.getElementById('0').onmouseup=inicio0;
 })
 calculadora();
